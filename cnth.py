f026d87529c8dd3b91370280ce4c086ec21c9c5e
abil0 <hoenabil6@gmail.com>
2025-07-23 10:41:06 +0700

    first commit

---
+++ cnth.py
@@ -0,0 +1,25 @@
+#contoh 1
+print ("Nabil Rafif")
+
+#contoh 2
+print ("I am learning python basics.")
+
+#contoh 3
+# baris ini meminta user/pengguna memasukkan nama mereka dan menyimpan dalm variable yang disebut teman
+name = input("Siapa nama anda? : ")
+
+# baris ini mencetak salam menggunakan nama yang dimasukkan user/pengguna
+print("Halo, " + name + "!")
+
+#raw_input() selalu mengembalikan string
+#name = raw_input("what is your name? ")
+#print("Halo, " + name + "!")
+
+#contoh 4/5
+# baris ini menanyakan usia pengguna menyimpan input dalam variable 'usia'
+age = input("How old are you? ")
+# baris ini mencetak usia yang dimasukkan user/pengguna
+print(age + "years old!")
+
+fakultas = input("lu fakultas apa ege? ")
+print (fakultas +  "uemyeah awaw!")
